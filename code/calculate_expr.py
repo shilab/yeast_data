965544e84307743e853e8a608d05c84f6e785a94
Andrew Quitadamo <andrew.quitadamo@gmail.com>
2015-09-19 18:48:12 -0400

    Added script to average expr vals

---
+++ code/calculate_expr.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import sys
+import math
+
+filename = sys.argv[1]
+
+with open(filename, 'r') as f:
+    for line in f:
+        if "!Sample_title" in line:
+            header = line.rstrip()
+            ids = header.split("\t")
+            ids = ids[1:]
+            i=0
+            results = "id"
+            while i<len(ids):
+                results+="\t" + ids[i].split("Cy")[0]
+                i+=2
+            print(results)
+        else:
+            samples = line.rstrip().split('\t')
+            id = samples[0]
+            samples = samples[1:]
+            i=0
+            results=id+"\t"
+            while i<len(samples):
+                if (samples[i]=="NULL" or samples[i+1]=="NULL"):
+                    results+="\tNULL"
+                elif (samples[i]=="0.000000" or samples[i+1]=="0.000000"):
+                    results+="\t0"
+                else:
+                    results+="\t" + str((float(samples[i])+float(samples[i+1])/2))
+                i+=2
+            print(results)
